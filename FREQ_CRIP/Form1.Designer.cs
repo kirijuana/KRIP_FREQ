24d7d405eba075b21cc8ebae621f1caee2cad01e
JussyFrussy <sazkirand@mail.ru>
2019-06-06 11:50:55 +0700

    Добавьте файлы проекта.

---
+++ FREQ_CRIP/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace FREQ_CRIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.ShifrText = new System.Windows.Forms.TextBox();
+            this.Text = new System.Windows.Forms.TextBox();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.button_unShifr = new System.Windows.Forms.Button();
+            this.button_Shifr = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog4 = new System.Windows.Forms.OpenFileDialog();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ShifrText
+            // 
+            this.ShifrText.Location = new System.Drawing.Point(196, 305);
+            this.ShifrText.Name = "ShifrText";
+            this.ShifrText.Size = new System.Drawing.Size(456, 20);
+            this.ShifrText.TabIndex = 14;
+            // 
+            // Text
+            // 
+            this.Text.Location = new System.Drawing.Point(196, 125);
+            this.Text.Name = "Text";
+            this.Text.Size = new System.Drawing.Size(456, 20);
+            this.Text.TabIndex = 13;
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(148, 211);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(189, 20);
+            this.keyBox.TabIndex = 12;
+            // 
+            // button_unShifr
+            // 
+            this.button_unShifr.Location = new System.Drawing.Point(371, 232);
+            this.button_unShifr.Name = "button_unShifr";
+            this.button_unShifr.Size = new System.Drawing.Size(124, 23);
+            this.button_unShifr.TabIndex = 11;
+            this.button_unShifr.Text = "Расшифровать";
+            this.button_unShifr.UseVisualStyleBackColor = true;
+            // 
+            // button_Shifr
+            // 
+            this.button_Shifr.Location = new System.Drawing.Point(371, 189);
+            this.button_Shifr.Name = "button_Shifr";
+            this.button_Shifr.Size = new System.Drawing.Size(124, 23);
+            this.button_Shifr.TabIndex = 10;
+            this.button_Shifr.Text = "Зашифровать";
+            this.button_Shifr.UseVisualStyleBackColor = true;
+            this.button_Shifr.Click += new System.EventHandler(this.button_Shifr_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // openFileDialog4
+            // 
+            this.openFileDialog4.FileName = "openFileDialog2";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(658, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(724, 605);
+            this.chart1.TabIndex = 15;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1445, 629);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.ShifrText);
+            this.Controls.Add(this.Text);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.button_unShifr);
+            this.Controls.Add(this.button_Shifr);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ShifrText;
+        private System.Windows.Forms.TextBox Text;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.Button button_unShifr;
+        private System.Windows.Forms.Button button_Shifr;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ FREQ_CRIP/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace FREQ_CRIP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Shifr_Click(object sender, EventArgs e)
+        {
+            char[] text = Text.Text.ToCharArray();
+            char[] shifr = Text.Text.ToCharArray();
+            char[] ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789".ToCharArray();
+            char[] alpha = "abcdefghijklmnopqrstuvwxyz 123456789".ToCharArray();
+            char[] alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+            char[] ALPH = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+            char[] ALPH_freq = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+
+            openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            string fileText = System.IO.File.ReadAllText(filename, Encoding.GetEncoding(1251));
+            
+            double[] freq_alph = { 0.062, 0.014, 0.038, 0.013, 0.025, 0.072, 0.001, 0.007, 0.016, 0.062, 0.010, 0.028, 0.035, 0.026, 0.053, 0.090, 0.023, 0.040, 0.045, 0.053, 0.021, 0.002, 0.009, 0.004, 0.012, 0.006, 0.003, 0.014, 0.016, 0.001, 0.003, 0.006, 0.018};
+            int[] count_symbols_text = new int[33];
+            int length_text = 0;
+            for(int i = 0; i < fileText.Length; i++)
+            {
+                for (int j = 0; j < alph.Length; j++)
+                {
+                    if(ALPH[j] == fileText[i])
+                    {
+                        count_symbols_text[j]++;
+                        length_text++;
+                        break;
+                    }
+                }
+            }
+
+            int[] freq_count_symbols = new int[33]; 
+
+            for(int i = 0; i < alph.Length; i++)
+            {
+                freq_count_symbols[i] = (int)Math.Truncate(freq_alph[i] * length_text);
+            }
+
+            int temp;
+            char temp_char = ' ';
+            for (int i = 0; i < freq_count_symbols.Length - 1; i++)
+            {
+                bool f = false;
+
+                for (int j = 0; j < freq_count_symbols.Length - i - 1; j++)
+                {
+                    if (freq_count_symbols[j + 1] > freq_count_symbols[j])
+                    {
+                        f = true;
+                        temp = freq_count_symbols[j + 1];
+                        temp_char = ALPH_freq[j + 1];
+                        freq_count_symbols[j + 1] = freq_count_symbols[j];
+                        ALPH_freq[j + 1] = ALPH_freq[j];
+                        freq_count_symbols[j] = temp;
+                        ALPH_freq[j] = temp_char;
+                    }
+                }
+                if (!f) break;
+            }
+    
+            for (int i = 0; i < count_symbols_text.Length - 1; i++)
+            {
+                bool f = false;
+                for (int j = 0; j < count_symbols_text.Length - i - 1; j++)
+                {
+                    if (count_symbols_text[j + 1] > count_symbols_text[j])
+                    {
+                        f = true;
+                        temp = count_symbols_text[j + 1];                   
+                        count_symbols_text[j + 1] = count_symbols_text[j];
+                        count_symbols_text[j] = temp;
+                        temp_char = ALPH[j + 1];
+                        ALPH[j + 1] = ALPH[j];
+                        ALPH[j] = temp_char;
+                    }
+                }
+                if (!f) break;
+            }
+ 
+            // Set palette.
+            this.chart1.Palette = ChartColorPalette.Excel;
+
+            for (int i = 0; i < count_symbols_text.Length; i++)
+            {              
+                // Add series.
+                Series series = this.chart1.Series.Add(ALPH[i].ToString());
+
+                // Add point.
+                series.Points.Add(count_symbols_text[i]);
+            }
+
+
+
+            char[] fileText_to_char = fileText.ToCharArray();
+            for(int i = 0; i < fileText_to_char.Length; i++)
+            {
+                for(int j = 0; j < ALPH.Length; j++)
+                {
+                    if(fileText_to_char[i] == ALPH[j])
+                    {
+                        fileText_to_char[i] = ALPH_freq[j];
+                        break;
+                    }
+                }
+            }
+            string filetext_str = new string(fileText_to_char);
+
+            saveFileDialog1.ShowDialog();
+            string filename2 = saveFileDialog1.FileName;
+            StreamWriter streamwriter = new System.IO.StreamWriter(filename2, false, System.Text.Encoding.GetEncoding("utf-8"));
+            streamwriter.Write(filetext_str);
+            streamwriter.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
